0a993d76cf0cdc0b6acdeba464198a1ffd7268c5
qbuxman <quentin.buxman@gmail.com>
2025-12-03 23:49:42 +0100

    ajustement role et test coverage

--- backend/contracts/Box10.sol
+++ backend/contracts/Box10.sol
@@ -12,6 +12,7 @@ error NonZeroValue();
 contract Box10 is ERC20, AccessControl {
     // Roles
     bytes32 public constant DISTRIBUTOR_ROLE = keccak256("DISTRIBUTOR_ROLE");
+    bytes32 public constant CRITICAL_DISTRIBUTOR_ROLE = keccak256("CRITICAL_DISTRIBUTOR_ROLE");
 
     // Supply
     uint public constant TOTAL_SUPPLY = 1_000_000_000 * 10**18;
@@ -20,17 +21,21 @@ contract Box10 is ERC20, AccessControl {
     event Distribute(address _to, uint _amount, string _activity);
     event TransferToken(address _from, address _to, uint _amount);
     event MintToken(address _from, address _to, uint _amount);
-    event BurnToken(address _from, address _to, uint _amount);
+    event BurnToken(address _from, uint _amount);
     event NewDistributorAdded(address _by, address _newDistributor);
     event DistributorRevoked(address _by, address _distributor);
 
-    constructor(address _admin, address _distributor) ERC20("BOX10 Token", "BOX10") {
+    constructor(address _admin, address _criticalDistributor, address _distributor) ERC20("BOX10 Token", "BOX10") {
         _grantRole(DEFAULT_ADMIN_ROLE, _admin);
+        _grantRole(CRITICAL_DISTRIBUTOR_ROLE, _criticalDistributor);
         _grantRole(DISTRIBUTOR_ROLE, _distributor);
 
         _mint(address(this), TOTAL_SUPPLY);
     }
-
+    /*
+     * @dev Add DISTRIBUTOR_ROLE to an address. Only account who has DEFAULT_ADMIN_ROLE role can use it
+     * @param _distributor Address whose role is added
+     */
     function addDistributor(address _distributor) external onlyRole(DEFAULT_ADMIN_ROLE) {
         require(_distributor != address(0), NonZeroAddress());
 
@@ -38,7 +43,10 @@ contract Box10 is ERC20, AccessControl {
 
         emit NewDistributorAdded(msg.sender, _distributor);
     }
-
+    /*
+     * @dev Remove DEFAULT_ADMIN_ROLE to an address. Only account who has DEFAULT_ADMIN_ROLE role can use it
+     * @param _distributor Address whose role is deleted
+     */
     function removeDistributor(address _distributor) external onlyRole(DEFAULT_ADMIN_ROLE) {
         require(_distributor != address(0), NonZeroAddress());
 
@@ -46,11 +54,22 @@ contract Box10 is ERC20, AccessControl {
 
         emit DistributorRevoked(msg.sender, _distributor);
     }
-
-    function isDistributor(address account) external view returns (bool) {
-        return hasRole(DISTRIBUTOR_ROLE, account);
+    /*
+     * @dev Function that check if address has DISTRIBUTOR_ROLE role
+     * @param _address Address to check
+     * @return true if address has DISTRIBUTOR_ROLE role
+     */
+    function isDistributor(address _address) external view returns (bool) {
+        return hasRole(DISTRIBUTOR_ROLE, _address);
     }
-
+    /*
+     * @dev Function that check if address has CRITICAL_DISTRIBUTOR_ROLE role
+     * @param _address Address to check
+     * @return true if address has CRITICAL_DISTRIBUTOR_ROLE role
+     */
+    function isCriticalDistributor(address _address) external view returns (bool) {
+        return hasRole(CRITICAL_DISTRIBUTOR_ROLE, _address);
+    }
     /*
      * @dev Function to send Box10 token to an address (cannot burn or mint)
      * @param _to Address to send token
@@ -68,7 +87,6 @@ contract Box10 is ERC20, AccessControl {
 
         emit Distribute(_to, _amount, _activity);
     }
-
     /*
      * @dev Get available supply of Box10 token
      * @return Balance of contract (not in wei)
@@ -76,35 +94,47 @@ contract Box10 is ERC20, AccessControl {
     function availableSupply() external view returns (uint) {
         return balanceOf(address(this)) / 10**decimals();
     }
-
-    function burn(uint _amount) external {
+    /*
+     * @dev Burn token
+     * @param _amount Amount of token to burn
+     */
+    function burnToken(uint _amount) external {
         require(_amount > 0, NonZeroValue());
 
-        _update(msg.sender, address(0), _amount);
+        _burn(msg.sender, _amount);
 
-        emit BurnToken(msg.sender, address(0), _amount);
+        emit BurnToken(msg.sender, _amount);
     }
-
     /*
      * @dev Override _update function from ERC20.sol to avoid transfer between user
+     * @param _from
+     * @param _to
+     * @param _amount
      */
     function _update(address _from, address _to, uint256 _amount) internal virtual override {
+        require(_amount > 0, NonZeroValue());
+
         // Mint
         if (_from == address(0)) {
             super._update(_from, _to, _amount);
+
             emit MintToken(_from, _to, _amount);
+
             return;
         }
+
         // Burn
         if (_to == address(0)) {
             super._update(_from, _to, _amount);
-            emit BurnToken(_from, _to, _amount);
+            emit BurnToken(_from, _amount);
             return;
         }
+
         // Transfer
         require(_from == address(this) || hasRole(DEFAULT_ADMIN_ROLE, _from), UserCannotTransferToken());
 
         super._update(_from, _to, _amount);
+
         emit TransferToken(_from, _to, _amount);
     }
 }
--- backend/test/Box10.ts
+++ backend/test/Box10.ts
@@ -8,6 +8,7 @@ describe("Box10", function () {
     let contract: any;
     let admin: SignerWithAddress;
     let distributor: SignerWithAddress;
+    let criticalDistributor: SignerWithAddress;
     let firstUser: SignerWithAddress;
     let secondUser: SignerWithAddress;
     let actions = {
@@ -18,9 +19,9 @@ describe("Box10", function () {
     const zeroAddress = '0x0000000000000000000000000000000000000000';
 
     before(async () => {
-        [admin, distributor, firstUser, secondUser] = await ethers.getSigners();
+        [admin, criticalDistributor, distributor, firstUser, secondUser] = await ethers.getSigners();
 
-        contract = await ethers.deployContract("Box10", [admin.address, distributor.address]);
+        contract = await ethers.deployContract("Box10", [admin.address, criticalDistributor, distributor.address]);
     })
 
     it("Should mint correctly supply of token", async () => {
@@ -46,16 +47,55 @@ describe("Box10", function () {
         });
     });
 
-    describe("_update", () => {
-        it("Should owner burn correctly", async () => {
-            const amount = 100n;
-            let totalSupplyBefore = await contract.totalSupply();
+    describe("addDistributor", () => {
+        it("Should add distributor", async () => {
+            await expect(contract.connect(admin).addDistributor(firstUser.address))
+                .to.emit(contract, "NewDistributorAdded")
+                .withArgs(admin.address, firstUser.address);
 
-            await contract.connect(firstUser).burn(amount);
+            expect(await contract.isDistributor(firstUser.address)).to.be.true;
+        });
 
-            let totalSupplyAfter = await contract.totalSupply();
+        it("Should revert id non admin try to add distributor", async () => {
+            await expect(
+                contract.connect(firstUser).addDistributor(secondUser.address)
+            ).revertedWithCustomError(contract, "AccessControlUnauthorizedAccount");
 
-            expect(totalSupplyAfter).to.be.lessThan(totalSupplyBefore);
+            expect(await contract.isDistributor(secondUser.address)).to.be.false;
         });
     });
+
+    describe("removeDistributor", () => {
+        it("Should remove distributor", async () => {
+            expect(await contract.isDistributor(firstUser.address)).to.be.true;
+
+            await expect(await contract.connect(admin).removeDistributor(firstUser.address))
+                .to.emit(contract, "DistributorRevoked")
+                .withArgs(admin.address, firstUser.address);
+
+            expect(await contract.isDistributor(firstUser.address)).to.be.false;
+        });
+
+        it("Should revert id non admin try to remove distributor", async () => {
+            await expect(
+                contract.connect(firstUser).removeDistributor(secondUser.address)
+            ).revertedWithCustomError(contract, "AccessControlUnauthorizedAccount");
+
+            expect(await contract.isDistributor(secondUser.address)).to.be.false;
+        });
+    });
+
+    describe('isCriticalDistributor', () => {
+        it('Should return true if distributor distributor is critical', async () => {
+            expect(await contract.isCriticalDistributor(criticalDistributor.address)).to.be.true;
+        })
+    });
+
+    describe("burn", () => {
+        it("Should burn positive amount of token and emit BurnToken event", async () => {
+            await expect(contract.connect(firstUser).burnToken(10))
+                .to.emit(contract, "BurnToken")
+                .withArgs(firstUser.address, 10);
+        })
+    })
 });
