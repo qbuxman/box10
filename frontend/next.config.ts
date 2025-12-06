4abaa391e8b2d239475279907104f1533a593579
qbuxman <quentin.buxman@gmail.com>
2025-12-06 20:45:42 +0100

    build

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  serverExternalPackages: ['pino', 'thread-stream', 'pino-pretty'],
+  serverExternalPackages: ['pino', 'thread-stream', 'pino-pretty', 'viem'],
   transpilePackages: ['@walletconnect/universal-provider'],
 };
 
