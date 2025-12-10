b7f4702899b84eb2a2fd382a046f517315787770
qbuxman <quentin.buxman@gmail.com>
2025-12-11 00:34:39 +0100

    changment deploy address

--- backend/ignition/modules/Box10.ts
+++ backend/ignition/modules/Box10.ts
@@ -1,7 +1,9 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
 export default buildModule("Box10Module", (m) => {
-    const adminWallet = '0x2C3F167288903caB856138845B9B3065684CF0D9';
+    const adminSafeWallet = '0x2C3F167288903caB856138845B9B3065684CF0D9';
+    const adminWallet = '0x948DB68A78b03aa8399E28Fb4D78D109D33d32CF';
+
     const criticalDistributorWallet = '0x948DB68A78b03aa8399E28Fb4D78D109D33d32CF';
     const distributorWallet = '0xe45437cab72d0139c4d72bf7682f0e1b303bb6b0';
 
